aaca01b7b16f353461620b0357db16a15593bacd
lsk <949919743@qq.com>
2020-08-23 23:43:56 +0800

    ir

--- applet2/pages/report/irReport.vue
+++ applet2/pages/report/irReport.vue
@@ -1,11 +1,207 @@
 <template>
 	<view class="page">
-		
+		<view class="head-box">
+			<view class="avatar">
+				<image src="../../static/h5icon/V.png" class="car-icon" mode=""></image>
+			</view>
+			<view class="right-content">
+				<view style="color: #232323;font-size: 30upx;">订单号: RP1288677879189606400</view>
+				<view style="font-size: 26upx; color: #cccccc;">WAUAGD4L0CD032131</view>
+				<view class="right-item"> <text v-for="(item,index) in headList" :key="index" :class="{'active': item.active}" class="right-item-label">{{item.label}}</text> </view>
+			</view>
+		</view>
+		<view class="update-time">
+			<text>报告更新时间:</text>
+			<text style="color: #ccc;">2020-07-30 11:28:56.0</text>
+		</view>
+		<view class="title-box">
+			<view class="icon"></view>
+			<view class="title">基础信息</view>
+		</view>
+		<view class="table-box">
+			<view class="table-item">
+				<view class="table-label">车险次数</view>
+				<view class="table-value">1</view>
+			</view>
+			<view class="table-item">
+				<view class="table-label">材料总金额/费用</view>
+				<view class="table-value">1</view>
+			</view>
+			<view class="table-item">
+				<view class="table-label">理赔总金额</view>
+				<view class="table-value">1</view>
+			</view>
+			<view class="table-item">
+				<view class="table-label">维修总金额/费用</view>
+				<view class="table-value">1</view>
+			</view>
+		</view>
+		<view class="title-box">
+			<view class="icon"></view>
+			<view class="title">概要信息</view>
+		</view>
+		<view class="time-line-box">
+			<view class="time-line-item">
+				<view class="time-line-item-index"><text class="time-line-item-index-label">1</text></view>
+				<view class="time-line-item-content"></view>
+			</view>
+		</view>
+		<view class="foot">
+			<view class="foot-title">
+				<text class="heng text"></text>
+				<text class="text" style="margin: 0 5upx;">报告诠释&免责条款</text>
+				<text class='heng text'></text>
+			</view>
+			<view class="foot-content">
+				此报告所包含内容及判断,仅基于截止到生成报告日期之前收集到的相关数据,关于此车其他车辆信息(包括但不限于在非4s店或非制造商授权店的维修信息)有可能未被采集或收录,本报告仅作参考.请结合实车勘验和试乘试驾体验,做出更精准的交易决策,如有疑问请联系客服咨询.
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	export default{
+		data(){
+			return {
+				headList: [
+					{
+						label: '维保',
+						active: false
+					},
+					{
+						label: '事故',
+						active: true
+					},
+					{
+						label: '投保',
+						active: false
+					},
+					{
+						label: '状态',
+						active: false
+					},
+					{
+						label: '违章',
+						active: false
+					},
+					{
+						label: '抵押',
+						active: false
+					},
+					
+				]
+			}
+		}
+	}
 </script>
 
 <style scoped lang="scss">
+	.page{
+		background-color: #F5F5F5;
+		width: 100vw;
+		height: 100vh;
+		overflow: auto;
+		.head-box{
+			display: flex;
+			align-items: center;
+			background-color: #fff;
+			padding: 20upx;
+			border-bottom: 1px solid #dcdcdc;
+			.avatar{
+				vertical-align: middle;
+				.car-icon{
+					width: 120upx;
+					height: 120upx;
+					border-radius: 8upx;
+					margin-right: 20upx;
+				}
+			}
+			.right-content{
+				.right-item{
+					display: flex;
+					align-items: center;
+					.right-item-label{
+						display: inline-block;
+						background-color: #e5e5e5;
+						color: #333333;
+						margin-right: 10upx;
+						padding: 5upx 10upx;
+						border-radius: 8upx;
+						font-size: 24upx;
+						&.active{
+							color: #FFFFFF;
+							background-color: #007AFF;
+						}
+					}
+				}
+			}
+		}
+		.update-time{
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			font-size: 26upx;
+			padding: 20upx;
+			background-color: #FFFFFF;
+		}
+		.title-box{
+			padding: 20upx;
+			margin: 14upx 0;
+			font-size: 30upx;
+			display: flex;
+			align-items: center;
+			justify-content: flex-start;
+			background-color: #FFFFFF;
+			.icon{
+				width: 12upx;
+				background-color: #007AFF;
+				height: 30upx;
+				margin-right: 10upx;
+			}
+		}
+		.table-box{
+			background-color: #FFFFFF;
+			margin: 0 30upx;
+			.table-item{
+				display: flex;
+				align-items: center;
+				border-bottom: 1px solid #F5F5F5;
+				font-size: 26upx;
+				.table-label{
+					border-right: 1px solid #F5F5F5;
+					width: 200upx;
+					padding: 20upx;
+				}
+				.table-value{
+					padding: 20upx;
+				}
+			}
+		}
+		.foot{
+			margin: 0 20upx 20upx 20upx;
+			padding: 20upx 0;
+			font-size: 26upx;
+			color: #666;
+			background-color: #FFFFFF;
+			.foot-title{
+				display: flex;
+				align-items: center;
+				.text{
+					display: inline-block;
+					flex: 1;
+					&.heng{
+						height: 2px;
+						background-color: #F5F5F5;
+					}
+				}
+			}
+			.foot-content{
+				margin-top: 20upx;
+				padding: 0 20upx;
+			}
+		}
+		.time-line-box{
+			
+		}
+	}
 </style>
